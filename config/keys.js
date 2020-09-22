a2ddc09cbfa83363147f80abda0f0c99a056ee37
Stephane WAFFO <guywaffo@gmail.com>
2020-09-22 23:56:29 +0200

    DEMO POUR CLIENT FANDIO

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
-    //mongoURI: "mongodb://localhost:27017/rh_bd",
+  //mongoURI: "mongodb://localhost:27017/rh_bd",
 
-        mongoURI: "mongodb://localhost:27017/bd_odicam",
+  //     mongoURI: "mongodb://localhost:27017/bd_odicam",
 
-
-    //mongoURI:"mongodb://odicam:odicam2020@cluster0-shard-00-00-eql0l.mongodb.net:27017,cluster0-shard-00-01-eql0l.mongodb.net:27017,cluster0-shard-00-02-eql0l.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
-  };
+  mongoURI:
+    "mongodb://odicam:odicam2020@cluster0-shard-00-00-eql0l.mongodb.net:27017,cluster0-shard-00-01-eql0l.mongodb.net:27017,cluster0-shard-00-02-eql0l.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
+};
